6edb866e2fd2aaa97125762bbcd69dd7f1f22235
Ubald de Bellabre <ubald.de-bellabre@external.total.com>
2022-08-17 16:44:15 +0200

    Use standard library for tests

---
+++ deps.test.ts
@@ -0,0 +1,8 @@
+export {
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.141.0/testing/bdd.ts";
+export { expect, mock } from "https://deno.land/x/expect@v0.2.9/mod.ts";
